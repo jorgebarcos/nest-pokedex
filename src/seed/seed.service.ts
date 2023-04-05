a8a086cd07cf282a74a9e92df69f28e9bd4d240b
jorgebarcos <jorgebarcos88@gmail.com>
2023-04-05 20:20:47 -0300

    Realizar petición http desde Nest

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,32 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed Executed';
+  constructor(private readonly httpService: HttpService) {}
+
+  async executeSeed() {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .pipe(
+          catchError((error: AxiosError) => {
+            console.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      console.log({ name, no });
+    });
+
+    return data;
   }
 }
